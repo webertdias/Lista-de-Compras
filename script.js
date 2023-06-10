889077cf579c33349cbc96f4649f249ba273feb4
Webert Dias <wdwebert@gmail.com>
2023-06-10 19:09:45 -0300

    Funcionamento do segundo item da lista com javascript

--- script.js
+++ script.js
@@ -12,13 +12,13 @@ btn.addEventListener('click', () => {
 
 let cleaning = document.querySelector('#cleaning');
 let btn1 = document.querySelector('#btn1');
-let pa = document.querySelector('#pa');
+let listCleaning = document.querySelector('#listCleaning');
 
 btn1.addEventListener('click', () => {   
-    item  = '<li>' + product.value + '</li>';
-    list.innerHTML +=item;
-    product.value = '';
-    product.focus();
+    item  = '<li>' + cleaning.value + '</li>';
+    bla.innerHTML +=item;
+    cleaning.value = '';
+    cleaning.focus();
 });
 
 
