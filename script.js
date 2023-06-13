172c77598835890209acda37d536cfff8ab00c9b
Webert Dias <wdwebert@gmail.com>
2023-06-12 21:25:24 -0300

    Renomeando variaveis de lista de limpeza

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
+//Mercearia//
+
 let product = document.querySelector('#product');
 let btn = document.querySelector('#btn');
-let list = document.querySelector('ul');
+let list = document.querySelector('#listProduct');
 
 
 btn.addEventListener('click', () => {   
@@ -10,13 +12,15 @@ btn.addEventListener('click', () => {
     product.focus();
 });
 
+//Limpeza e Higiene//
+
 let cleaning = document.querySelector('#cleaning');
 let btn1 = document.querySelector('#btn1');
 let listCleaning = document.querySelector('#listCleaning');
 
 btn1.addEventListener('click', () => {   
     item  = '<li>' + cleaning.value + '</li>';
-    bla.innerHTML +=item;
+    listCleaning.innerHTML +=item;
     cleaning.value = '';
     cleaning.focus();
 });
